36bbf4901f80ba05020dc612c9cbc38eb880e29b
mohmedy <mohmedyusoof.as@cognizant.com>
2014-07-30 17:27:04 +0100

    Testing two

---
+++ src/main/groovy/GebTestingClass.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ee
+ * Date: 30/07/14
+ * Time: 17:26
+ * To change this template use File | Settings | File Templates.
+ */
+class GebTestingClass {
+}
